b38815730833cf228db2a722f5e7b872db5e19f3
Tomo-ja <tomonao.hj@gmail.com>
2022-06-18 15:50:37 -0700

    create static header

---
+++ src/Component/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+	<header className='header'>
+		<h1 className='header__title'>Converstion Starter</h1>
+		<h2 className='header__sub-title'>Kick start with fun sentence for chat app</h2>
+	</header>
+  )
+}
