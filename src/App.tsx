4fe36b7c057f75cfe2b7284f415dde90f3a765c1
Tomo-ja <tomonao.hj@gmail.com>
2022-06-18 15:53:18 -0700

    import axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Component/Header';
+import Hero from './Component/Hero';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
     </div>
   );
 }
