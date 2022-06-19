375ccb4e026c0881eb9aca03bbebf070217b2db6
Tomo-ja <tomonao.hj@gmail.com>
2022-06-19 10:25:42 -0700

    set up to be able to import svg

--- src/Component/Joke.tsx
+++ src/Component/Joke.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import PropTypes, { InferProps } from 'prop-types'
+import copyIconSolid from '../images/icon_copy_solid.svg'
 
 export default function Joke ({ phrase }: InferProps<typeof Joke.propTypes>) {
   return (
 	<div className='joke'>
 		<p className='joke__phrase'>{phrase}</p>
-		
+		<img src={copyIconSolid} />
 	</div>
   )
 }
