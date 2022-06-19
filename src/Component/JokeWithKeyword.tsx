4ca12abf850eb7cc20f8fd3d83b1b39a374944ad
Tomo-ja <tomonao.hj@gmail.com>
2022-06-19 15:54:19 -0700

    create section components

---
+++ src/Component/JokeWithKeyword.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function JokeWithKeyword() {
+  return (
+	<div>JokeWithKeyword</div>
+  )
+}
--- src/Component/Main.tsx
+++ src/Component/Main.tsx
@@ -1,13 +1,25 @@
 import React, { useState } from 'react'
 import Tabbar from './Tabbar'
+import JokeWithKeyword from './JokeWithKeyword'
+import Prise from './Prise'
+import Insult from './Insult'
+import History from './History'
 
 export default function Main() {
 
 	const [pageIndex, setPageIndex] = useState(0)
 
+	const contentsComponents = [
+		<JokeWithKeyword />,
+		<Prise />,
+		<Insult />,
+		<History />
+	]
+
   return (
 	<main className='main'>
 		<Tabbar pageIndex={ pageIndex } setPageIndex = { setPageIndex }/>
+		{ contentsComponents[pageIndex]}
 	</main>
   )
 }
