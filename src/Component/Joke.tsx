ca0c9181a50ba4e09b9d56f634ca174296a4a23d
Tomo-ja <tomonao.hj@gmail.com>
2022-06-19 11:08:51 -0700

    create toggle function on Joke compornent

--- src/Component/Joke.tsx
+++ src/Component/Joke.tsx
@@ -1,12 +1,32 @@
 import React from 'react'
 import PropTypes, { InferProps } from 'prop-types'
 import copyIconSolid from '../images/icon_copy_solid.svg'
+import copyIconBorder from '../images/icon_copy_border.svg'
+import starIconSolid from '../images/icon_star_solid.svg'
+import starIconBorder from '../images/icon_star_border.svg'
 
 export default function Joke ({ phrase }: InferProps<typeof Joke.propTypes>) {
   return (
 	<div className='joke'>
 		<p className='joke__phrase'>{phrase}</p>
-		<img src={copyIconSolid} />
+		<div className="joke__btn-container">
+			<div className="joke__btn-container_btn">
+				<img 
+					src={copyIconBorder} 
+					alt="copy icon"
+					onMouseOver={e => {e.currentTarget.src = copyIconSolid}}
+					onMouseOut = {e => {e.currentTarget.src = copyIconBorder}}
+				/>
+			</div>
+			<div className="joke__btn-container_btn">
+				<img 
+					src={starIconBorder} 
+					alt="copy icon"
+					onMouseOver={e => {e.currentTarget.src = starIconSolid}}
+					onMouseOut = {e => {e.currentTarget.src = starIconBorder}}
+				/>
+			</div>
+		</div>
 	</div>
   )
 }
