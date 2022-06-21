64c8963329e9f8bc44189d69c6574ccce3ea1363
Tomo-ja <tomonao.hj@gmail.com>
2022-06-21 16:32:58 -0700

    write some sentence

--- src/Component/Prise.tsx
+++ src/Component/Prise.tsx
@@ -6,12 +6,13 @@ export default function Prise() {
     	<form className='MAIN-SECTION__CONTENT'>
 		  </form>
 		<div className='MAIN-SECTION__CONTENT'>
-    <h2  className='MAIN-SECTION__CONTENT__TITLE'></h2>
-			<h3 className='MAIN-SECTION__CONTENT__SUBTITLE'></h3>
-			<p className='MAIN-SECTION__CONTENT__P'></p>
-			<h3 className='MAIN-SECTION__CONTENT__SUBTITLE'></h3>
+		<h2  className='MAIN-SECTION__CONTENT__TITLE'>Generate mega praise of a person</h2>
+			<h3 className='MAIN-SECTION__CONTENT__SUBTITLE'>Name:</h3>
+			<p className='MAIN-SECTION__CONTENT__P'>Name of person or name you call the person</p>
+			<h3 className='MAIN-SECTION__CONTENT__SUBTITLE'>Sentence:</h3>
 			<p className='MAIN-SECTION__CONTENT__P'>
-      </p>
+				Sentence will be included in a praise. You should input something you want to say good about 
+      		</p>
 		</div>
   </section>
   )
