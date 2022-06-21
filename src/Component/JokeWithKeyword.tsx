c5d2868306a04d203d8b0f0006e9263c23513cf5
Tomo-ja <tomonao.hj@gmail.com>
2022-06-20 20:50:18 -0700

    create exprenation

--- src/Component/JokeWithKeyword.tsx
+++ src/Component/JokeWithKeyword.tsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import interests from '../Data/interests'
 
 export default function JokeWithKeyword() {
   return (
-	<section>JokeWithKeyword</section>
+	<section>
+		<form>
+			<div className="keyword">
+				<p>Keyword</p>
+				<input type='text' />
+				<input type="text" />
+			</div>
+			<div>
+				<div>Choose interests</div>
+				<div>
+					{ Object.values(interests).map( keyword => (
+						<p>{keyword}</p>
+					))}
+				</div>
+			</div>
+		</form>
+		<div>
+			<h2>Generate joke with keyword and their interests</h2>
+			<h3>Keyword:</h3>
+			<p>Keyword will be included in jokes. this is optional(maxmam 2 words)</p>
+			<h3>Interests:</h3>
+			<p>This is category of jokes that will be generate. Recommend pick one from person's profile that you will use a joke. this is required </p>
+		</div>
+	</section>
   )
 }
---
+++ src/Data/interests.ts
@@ -0,0 +1,13 @@
+
+const interests = {
+	1: 'traveling',
+	2: 'exercise',
+	3: 'theater',
+	4: 'dancing',
+	5: 'cooking',
+	6: 'pets',
+	7: 'photography',
+	8: 'sports'
+}
+
+export default interests
