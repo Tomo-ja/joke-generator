01d30cbea166377937480b42e549dc0b86a29632
Tomo-ja <tomonao.hj@gmail.com>
2022-06-21 19:29:49 -0700

    create form on praise and insult jokes

--- src/Component/Insult.tsx
+++ src/Component/Insult.tsx
@@ -3,8 +3,27 @@ import React from 'react'
 export default function Insult() {
   return (
     <section className='MAIN-SECTION'>
-      <form className='MAIN-SECTION__CONTENT'>
-      </form>
+    	<form className='MAIN-SECTION__CONTENT'>
+			<p className="MAIN-SECTION__CONTENT-FORM__LABEL">Name</p>
+			<input 
+				type='text'
+				className="MAIN-SECTION__CONTENT-FORM__INPUT"
+				placeholder='Name is required'
+				required
+			/>
+			<p className="MAIN-SECTION__CONTENT-FORM__LABEL">Sentence</p>
+			<textarea
+				rows={5}
+				className="MAIN-SECTION__CONTENT-FORM__INPUT"
+				placeholder='One line sentence is required'
+				required
+			/>
+			<button
+				className="MAIN-SECTION__CONTENT-FORM__SUBMIT"
+			>
+				Generate
+			</button>
+		</form>
        <div className='MAIN-SECTION__CONTENT'>
         <h2  className='MAIN-SECTION__CONTENT__TITLE'>Generate insult phrase of a person</h2>
         <h3 className='MAIN-SECTION__CONTENT__SUBTITLE'>Name:</h3>
--- src/Component/Prise.tsx
+++ src/Component/Prise.tsx
@@ -4,7 +4,26 @@ export default function Prise() {
   return (
 	<section className='MAIN-SECTION'>
     	<form className='MAIN-SECTION__CONTENT'>
-		  </form>
+			<p className="MAIN-SECTION__CONTENT-FORM__LABEL">Name</p>
+			<input 
+				type='text'
+				className="MAIN-SECTION__CONTENT-FORM__INPUT"
+				placeholder='Name is required'
+				required
+			/>
+			<p className="MAIN-SECTION__CONTENT-FORM__LABEL">Sentence</p>
+			<textarea
+				rows={5}
+				className="MAIN-SECTION__CONTENT-FORM__INPUT"
+				placeholder='One line sentence is required'
+				required
+			/>
+			<button
+				className="MAIN-SECTION__CONTENT-FORM__SUBMIT"
+			>
+				Generate
+			</button>
+		</form>
 		<div className='MAIN-SECTION__CONTENT'>
 		<h2  className='MAIN-SECTION__CONTENT__TITLE'>Generate mega praise of a person</h2>
 			<h3 className='MAIN-SECTION__CONTENT__SUBTITLE'>Name:</h3>
