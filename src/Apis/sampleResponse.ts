5d2f94af806b0170314727c1560f77d6d2e5c529
Tomo-ja <tomonao.hj@gmail.com>
2022-06-22 12:45:48 -0700

    finish basic application"

--- src/Apis/sampleResponse.ts
+++ src/Apis/sampleResponse.ts
@@ -2,6 +2,7 @@
 const sampleData = {
 	random: {text: 'Chuck Norris knows who the fuck is Alice.'},
 	praise: {text: "Holy crap, Your drawing looks amazing. Peter, you godlike creature."},
+	insult: {text: "And there we go Peter, you moronic airhead, Your painting looks ugly.."},
 	long: { text : 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo deleniti rerum sapiente neque corporis optio iusto minus fugiat magni libero pariatur necessitatibus nesciunt ullam doloremque consequuntur dolorem illo, similique, omnis, non cum est officiis nisi error. Laudantium molestiae, ab porro nulla, maxime earum aperiam placeat rem, deserunt quos id laboriosam sapiente cum perspiciatis. Explicabo est modi, unde quae ut mollitia perspiciatis vero ea vel, veniam harum culpa. Odit qui explicabo deserunt, doloribus corrupti distinctio error. Perspiciatis, recusandae nulla sapiente quo similique eaque assumenda quod ullam labore at iste eveniet mollitia illum velit distinctio sed, architecto vel sit in, amet ea.'}
 }
 
--- src/Component/Insult.tsx
+++ src/Component/Insult.tsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useState, useContext} from 'react'
+import axios from '../Apis/axios'
+import request from '../Apis/Request'
+import { HistoryJokesContext, JokeModel, TalkResponseModel } from '../App'
+import Joke from './Joke'
+import sampleData from '../Apis/sampleResponse'
 
 export default function Insult() {
+
+	const { setJokesHistory } = useContext(HistoryJokesContext)
+
+	const [nameState, setNameState] = useState("")
+	const [sentence, setSentence] = useState("")
+	const [insultResult, setInsultResult] = useState<JokeModel>({phrase: "", favorite: false})
+	const [isSearched, setIsSearched] = useState(false)
+
+	async function getInsult(e: React.SyntheticEvent) {
+		e.preventDefault()
+		const params: URLSearchParams = new URLSearchParams([
+			['name', nameState],
+			['content', sentence]
+		])
+
+		setIsSearched(true)
+
+		try{
+			const insult: TalkResponseModel = await axios.get(request.fetchInsult, { params })
+			setInsultResult(()=>(
+				{phrase: insult.text, favorite: false}
+			))
+		}catch(e){
+			console.log(e)
+			setInsultResult(()=>(
+				{phrase: sampleData.insult.text, favorite: false}
+			))
+		}
+	}
+
   return (
     <section className='MAIN-SECTION'>
-    	<form className='MAIN-SECTION__CONTENT'>
+    	<form onSubmit={getInsult} className='MAIN-SECTION__CONTENT'>
 			<p className="MAIN-SECTION__CONTENT-FORM__LABEL">Name</p>
-			<input 
+			<input
+				onChange={e => setNameState(e.target.value)}
 				type='text'
 				className="MAIN-SECTION__CONTENT-FORM__INPUT"
 				placeholder='Name is required'
@@ -13,6 +49,7 @@ export default function Insult() {
 			/>
 			<p className="MAIN-SECTION__CONTENT-FORM__LABEL">Sentence</p>
 			<textarea
+				onChange={e => setSentence(e.target.value)}
 				rows={5}
 				className="MAIN-SECTION__CONTENT-FORM__INPUT"
 				placeholder='One line sentence is required'
@@ -24,14 +61,18 @@ export default function Insult() {
 				Generate
 			</button>
 		</form>
-       <div className='MAIN-SECTION__CONTENT'>
-        <h2  className='MAIN-SECTION__CONTENT__TITLE'>Generate insult phrase of a person</h2>
-        <h3 className='MAIN-SECTION__CONTENT__SUBTITLE'>Name:</h3>
-        <p className='MAIN-SECTION__CONTENT__P'>Name of person or name you call the person</p>
-        <h3 className='MAIN-SECTION__CONTENT__SUBTITLE'>Sentence:</h3>
-        <p className='MAIN-SECTION__CONTENT__P'>
-          Sentence will be included in a insult phrase. You should input something you want to say bad about 
-        </p>
-      </div>
+		{ isSearched ?
+			<Joke joke={insultResult} setJokeHistory={ setJokesHistory } />
+			:
+			<div className='MAIN-SECTION__CONTENT'>
+				<h2  className='MAIN-SECTION__CONTENT__TITLE'>Generate insult phrase of a person</h2>
+				<h3 className='MAIN-SECTION__CONTENT__SUBTITLE'>Name:</h3>
+				<p className='MAIN-SECTION__CONTENT__P'>Name of person or name you call the person</p>
+				<h3 className='MAIN-SECTION__CONTENT__SUBTITLE'>Sentence:</h3>
+				<p className='MAIN-SECTION__CONTENT__P'>
+				Sentence will be included in a insult phrase. You should input something you want to say bad about 
+				</p>
+		 	 </div>
+		}
 </section>
 )}
